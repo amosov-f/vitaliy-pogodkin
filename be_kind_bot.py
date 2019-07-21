774ad09ce5ed84942083274385f8b62e027351e0
Fedor Amosov <amosov.f@mail.ru>
2019-07-21 16:42:00 +0300

    Add files via upload

---
+++ be_kind_bot.py
@@ -0,0 +1,81 @@
+import json
+import logging
+from datetime import datetime
+
+import requests
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+
+
+def start(bot, update):
+    update.message.reply_text('Всем добра и быть добрее!')
+
+
+def pass_location(bot, update):
+    print(update.message.location)
+    location = update.message.location
+    lon = location.longitude
+    lat = location.latitude
+
+    result = get_weather(lat, lon)
+
+    answer = ''
+    for date, temp, condition in result:
+        day = datetime.strptime(date, '%Y-%m-%d')
+        order = day.weekday()
+        week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+        week_day = week[order]
+
+        sign = '+' if temp > 0 else ''
+
+        icons = {
+            'cloudy': '☁',
+            'clear': '🌞',
+            'partly-cloudy': '⛅',
+            'overcast-and-light-rain': '🌦',
+            'overcast': '🌪',
+            'cloudy-and-light-rain': '🌧'
+        }
+        icon = icons[condition] if condition in icons else f'не шмог {condition}'
+
+        answer += f'{week_day}: {sign}{temp}°C {icon}\n'
+
+    update.message.reply_text(answer)
+
+
+def get_weather(lat, lon):
+    url = f'https://api.weather.yandex.ru/v1/forecast?lat={lat}&lon={lon}'
+    print(url)
+    response = requests.get(url, headers={
+        'X-Yandex-API-Key': '116148fa-0672-4bd4-9c8e-925fb3e20c5d'
+    })
+    response.raise_for_status()
+    response_json = response.json()
+
+    result = []
+    for forecast in response_json['forecasts']:
+        date = forecast['date']
+        day_short = forecast['parts']['day_short']
+        temp = day_short['temp']
+        condition = day_short['condition']
+        result.append((date, temp, condition))
+
+    return result
+
+
+def start_bot():
+    updater = Updater("930754446:AAHMBGcbo07gitLHIYZ_JeReysaavX7RwAY")
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.location, pass_location))
+    updater.start_polling()
+    updater.idle()
+
+
+def main():
+    start_bot()
+
+
+if __name__ == '__main__':
+    main()
